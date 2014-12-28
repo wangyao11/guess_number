0e195dd87bdd64cccf318e6bb735153cbba9f8cc
ywang <657316992@qq.com>
2014-12-28 23:37:25 +0800

    update random-numbers-test.js bug

--- __tests__/random-numbers-test.js
+++ __tests__/random-numbers-test.js
@@ -7,8 +7,7 @@ describe('RandomNumber', function() {
     it('shuold return array contain four number and number not repeat ', function() {
       var _ = require('lodash');
       var RandomNumbers = require('../js/random-numbers');
-      var randomNumbers = new RandomNumbers(4);
-      var expectArray = randomNumbers.getRandomNumbers();
+      var expectArray = RandomNumbers.getRandomNumbers(4);
       var array = _.uniq(expectArray);
 
       expect(expectArray.length).toBe(4);
