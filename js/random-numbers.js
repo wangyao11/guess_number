4e35fab0268b11c2be9d8bef0a1ebd616e132b11
ywang <657316992@qq.com>
2014-12-28 17:50:04 +0800

    update random-numbers.ja bug

--- js/random-numbers.js
+++ js/random-numbers.js
@@ -6,8 +6,10 @@ function RandomNumbers(randomLength) {
 
 RandomNumbers.prototype.getRandomNumbers = function () {
   var numbers = [];
+
   while (numbers.length < this.randomLength) {
-    var randomNumber = _.random(0,10);
+    var randomNumber = _.random(0,9);
+
     if (!_.contains(numbers , randomNumber)) {
       numbers.push(randomNumber);
     }
